e6d08b49ee6fc61d25a320b0aba73ce1e9753c54
Jean Will <jcarloswill@gmail.com>
2017-04-06 09:22:04 -0300

    Correções

--- js/app.js
+++ js/app.js
@@ -3,15 +3,17 @@ var Item = function(x, y, speed, sprite) {
   this.y = y;
   this.speed = speed;
   this.sprite = sprite;
-}
+};
 
 Item.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // Inimigos que o jogador deve evitar
-var Enemy = function(x, y, speed) {
-    Item.call(this, x, y, speed, 'images/enemy-bug.png');
+var Enemy = function() {
+  Item.call(this, 0, 0, 0, 'images/enemy-bug.png');
+  this.calculateEnemyY();
+  this.calculateEnemySpeed();;
 };
 
 Enemy.prototype = Object.create(Item.prototype);
@@ -22,13 +24,13 @@ Enemy.prototype.update = function(dt) {
   this.x += this.speed * dt;
   this.checkCollisions();
   calculateAddEnemy();
-}
+};
 
 // Verifica se o inimigo colidiu com o personagem
-Enemy.prototype.checkCollisions = function () {
+Enemy.prototype.checkCollisions = function() {
   if (player.x >= this.x - 70 &&
-      player.x <= this.x + 70 &&
-      player.y == this.y) {
+    player.x <= this.x + 70 &&
+    player.y == this.y) {
     player.x = 202;
     player.y = 380;
 
@@ -40,16 +42,16 @@ Enemy.prototype.checkCollisions = function () {
   }
 
   if (this.x >= 500) {
-    this.y = calculateEnemyY();
+    this.calculateEnemyY();
     this.x = 0;
-    this.speed = calculateEnemySpeed();
+    this.calculateEnemySpeed();
   }
 };
 
 // Jogador
 var Player = function(x, y, speed) {
   Item.call(this, x, y, speed, 'images/char-boy.png');
-}
+};
 
 Player.prototype = Object.create(Item.prototype);
 Player.prototype.constructor = Player;
@@ -61,7 +63,7 @@ Player.prototype.update = function(dt) {
 
 // Atualiza a posição do jogador de acordo com o botão clicado
 Player.prototype.handleInput = function(key) {
-  switch(key) {
+  switch (key) {
     case 'up':
       this.y -= this.speed - 20;
       break;
@@ -76,61 +78,62 @@ Player.prototype.handleInput = function(key) {
       break;
     default:
       break;
-  };
+  }
+  ;
 };
 
 // Verifica se o jogador venceu o jogo chegando na água
 // Verifica também os limites da tela, para que o jogador não saia dela
 Player.prototype.checkWin = function() {
-  if (player.y <= 0) {
-    player.x = 202;
-    player.y = 380;
+  if (this.y <= 0) {
+    this.x = 202;
+    this.y = 380;
 
     level++;
     numEnemies++;
   }
 
-  if (player.y > 380) {
-    player.y = 380;
+  if (this.y > 380) {
+    this.y = 380;
   }
 
-  if (player.x < 2) {
-    player.x = 2;
+  if (this.x < 2) {
+    this.x = 2;
   }
 
-  if (player.x > 402) {
-    player.x = 402;
+  if (this.x > 402) {
+    this.x = 402;
   }
 };
 
 // Calcula aleatóriamente a posição do inimigo no eixo y
-var calculateEnemyY = function() {
+Enemy.prototype.calculateEnemyY = function() {
   var y = Math.floor(Math.random() * -3 + 1) * -1;
 
   if (y == 1) {
-    return 60;
+    this.y = 60;
   } else if (y == 2) {
-    return 140;
+    this.y = 140;
   } else {
-    return 220;
+    this.y = 220;
   }
-}
+};
 
 // Calcula aleatóriamente a velocidade do inimigo
-var calculateEnemySpeed = function() {
-  var speed = Math.floor(Math.random() * 500 );
+Enemy.prototype.calculateEnemySpeed = function() {
+  var speed = Math.floor(Math.random() * 500);
 
   if (speed < 50) {
-    speed = 50;
+    this.speed = 50;
+  } else {
+    this.speed = speed;
   }
-
-  return speed;
-}
+};
 
 // Adiciona um novo inimigo na tela
 var addEnemy = function() {
-  allEnemies.push(new Enemy(0, calculateEnemyY(), calculateEnemySpeed()));
-}
+  allEnemies.push(new Enemy());
+};
 
 // Adiciona, se possível, um novo inimigo na tela, mantendo o número de Inimigos
 // condizente com o nível atingido
@@ -138,7 +141,7 @@ var calculateAddEnemy = function() {
   if (allEnemies.length < numEnemies) {
     addEnemy();
   }
-}
+};
 
 var allEnemies = [];
 var player = new Player(202, 380, 100);
@@ -149,12 +152,12 @@ calculateAddEnemy();
 
 // Evento para verificar as teclas pressionadas
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
